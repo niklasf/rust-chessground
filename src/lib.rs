36e93ccc9d9fadfc0f4ef205a852a26364075218
Niklas Fiekas <niklas.fiekas@backscattering.de>
2018-06-05 13:34:59 +0200

    bump 0.8.0

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,7 @@
 
 //! A chessboard widget for Relm/GTK.
 
-#![doc(html_root_url = "https://docs.rs/chessground/0.7.0")]
+#![doc(html_root_url = "https://docs.rs/chessground/0.8.0")]
 
 #![warn(missing_debug_implementations)]
 
